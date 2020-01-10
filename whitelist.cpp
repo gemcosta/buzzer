7a0fe60de3748b017af7361babc28c48afd31b41
Gabe Medina <gemcosta@users.noreply.github.com>
2020-01-10 00:11:34 -0800

    Updated placeholders

--- whitelist.cpp
+++ whitelist.cpp
@@ -1,6 +1,6 @@
 exports.handler = function(context, event, callback) {
   // List all ALLOWED phone numbers in quotes and E.164 formatting, separated by a comma
-  let whitelist = event.whitelist || [ "+12066324139", "+15614528021" ];  
+  let whitelist = event.whitelist || [ "+15551234567", "+12129876543" ];  
   let twiml = new Twilio.twiml.VoiceResponse();
   let blocked = true;
   if (whitelist.length > 0) {
@@ -13,7 +13,7 @@ exports.handler = function(context, event, callback) {
   }
   else {
   // if the caller's number is not blocked, redirect to your existing webhook
-  twiml.redirect("https://handler.twilio.com/twiml/EH1ce25b6b00b133847306b84f644e307f");
+  twiml.redirect("https://handler.twilio.com/twiml/YOUR-WEBHOOK-URL");
   }
   callback(null, twiml);
 };
